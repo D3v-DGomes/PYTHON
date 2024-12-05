064dccb7ef57fede2a13256a3078680fa7adcc9e
D3v-DGomes <devgomes1223@hotmail.com>
2024-12-05 19:41:16 -0300

    Add files via upload

---
+++ PYTHON/logica_da_programacao/023-tupla.py
@@ -0,0 +1,16 @@
+'''
+TUPLAS: 
+--> São tipos de dados que permitem armazenar múltiplos valores em uma única variável, assim como listas.
+--> As tuplas são imutáveis. uma vez criadas, seus valores não podem ser alterados.
+
+CARACTERÍSTICAS:
+--> Imutáveis: Uma vez criada, você não pode modificar, adicionar ou remover elementos da tupla.
+--> Ordenadas: Os elementos têm uma ordem definida e essa ordem não pode ser alterada.
+--> Permite Duplicatas: As tuplas podem conter múltiplos elementos com o mesmo valor.
+'''
+
+tupla = (1,2,3,4,5)
+tupla_2 = ('João', 'Noé', 'Abel')
+
+print(f'Tupla = {tupla}')
+print(f'Tupla 2 = {tupla_2}')
