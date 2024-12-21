6e88ff8f57841afd1944109c5ea55e8df7131b79
D3v-DGomes <devgomes1223@hotmail.com>
2024-12-21 18:16:21 -0300

    Add files via upload

---
+++ PYTHON/books_python/int_a_prog_py/exercise004.py
@@ -0,0 +1,6 @@
+'''
+Make a program that displays your name on the screen
+'''
+
+name = 'David Silva'
+print(name)
---
+++ PYTHON/books_python/int_a_prog_py/exercise006.py
@@ -0,0 +1,10 @@
+'''
+Write a program that  calculates the sum of three variables and 
+prints the result on the screen
+'''
+
+a = 3
+b = 9
+c = 5
+sum_variables = a + b + c
+print(sum_variables)
