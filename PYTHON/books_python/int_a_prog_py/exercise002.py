72044b7797173be847204bf3a03c9ca34bf1c15d
D3v-DGomes <devgomes1223@hotmail.com>
2024-12-23 19:32:59 -0300

    Delete PYTHON/books_python/int_a_prog_py/exercise002.py

--- PYTHON/books_python/int_a_prog_py/exercise002.py
+++
@@ -1,9 +0,0 @@
-'''
-1- Type the following expression into the interpreter:
-
-10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2
-
-Try to solve the same calculation, using the priority of operations.
-'''
-
-print(((10 % 3) * (10 ** 2) + 1 -(10 * (4 / 2)))) 
