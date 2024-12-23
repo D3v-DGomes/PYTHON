e98cd5cfe1b5af0b028e89cd23e0aa8f85359741
D3v-DGomes <devgomes1223@hotmail.com>
2024-12-23 19:33:10 -0300

    Delete PYTHON/books_python/int_a_prog_py directory

--- PYTHON/books_python/int_a_prog_py/exercise003.py
+++
@@ -1,6 +0,0 @@
-'''
-1- First program with variables:
-'''
-a = 2   # a recieves 2
-b = 3   # b recieves 3
-print(a + b)    # sum between a and b
--- PYTHON/books_python/int_a_prog_py/exercise004.py
+++
@@ -1,6 +0,0 @@
-'''
-Make a program that displays your name on the screen
-'''
-
-name = 'David Silva'
-print(name)
